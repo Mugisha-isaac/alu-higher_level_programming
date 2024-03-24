2fe5e2857abd97f17051f3db0613f9fe63e3fcc9
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-03-24 22:08:20 +0300

    fix: check url startWith

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""_summary_
-- Write a Python script that
-- fetches https://intranet.hbtn.io/status.
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
 """
 import requests
 
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    if url.startswith('https://'):
+        url = "https://alu-intranet.hbtn.io/status"
 
-if _name_ == "_main_":
-    r = requests.get("https://intranet.hbtn.io/status")
+    res = requests.get(url)
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format("OK" if len(r.text) > 100 else r.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
