8040ceae674bc58877e0ecf982c8f2586b855e37
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-03-24 16:23:28 +0300

    fix: use requests to fetch url

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,10 +5,8 @@ Python script that fetches https://intranet.hbtn.io/status
 import requests
 
 if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    response = requests.get(url)
-    
-    print("Body response:")
-    print("\t- type: {}".format(type(response.text)))
-    print("\t- content: {}".format(response.text))
 
+    res = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
