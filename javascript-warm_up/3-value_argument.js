15f0cd23fe2660bb746f24fbecd0cf7401b89c3c
Mugisha-isaac <mugishaisaac0508@gmail.com>
2024-05-29 12:44:41 +0000

    fix: semistandrd issues

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const {argv} = require('node:process');
+const { argv } = require('node:process');
 
-console.log(argv.length -2 === 0 ? 'No argument' : argv[2]);
+console.log(argv.length - 2 === 0 ? 'No argument' : argv[2]);
--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const {argv} = require('node:process');
+const { argv } = require('node:process');
 
 console.log(Number(argv[2]) ? `My number: ${argv[2]}` : 'Not a number');
