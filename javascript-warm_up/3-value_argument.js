1b5f87ca8cfc03c69f55c389eb9b3bb6fff9f1b8
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-05-29 15:53:29 +0200

    fix: resolve conflicts

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,11 +3,7 @@ const { argv } = require("node:process");
 const [, , firstArg] = argv;
 
 if (firstArg === undefined) {
-<<<<<<< HEAD
   console.log("No argument");
-=======
-  console.log('No argument');
->>>>>>> d54ae80668f5472130050fd5fc93f7f5d2bee61f
 } else {
   console.log(firstArg);
 }
