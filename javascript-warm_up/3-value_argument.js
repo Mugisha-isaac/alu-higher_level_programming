e628ec3bd4f716aaf1a6598e98e4ac4c246bda9f
Mugisha-isaac <mugishaisaac0508@gmail.com>
2024-05-29 13:55:46 +0000

    fix: semistandard fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
+const { argv } = require('node:process');
 const [, , firstArg] = argv;
 
 if (firstArg === undefined) {
-  console.log("No argument");
+  console.log('No argument');
 } else {
   console.log(firstArg);
 }
